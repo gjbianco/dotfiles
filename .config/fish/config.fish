7e94f288ddc943a8bb7033069aba33bc77bbc133
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-01-29 14:27:27 -0500

    comment and add fish bang bang

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,12 @@
 set -x EDITOR vim
 set -x PATH $PATH:$HOME/bin
 
+# fundle plugins
 fundle plugin 'edc/bass'
+fundle plugin 'oh-my-fish/plugin-bang-bang'
 
 fundle init
+# end fundle plugins
 
+# auto run nvm use and making it "quiet"
 nvm use default >> /dev/null
