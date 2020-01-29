80a25c6639c55a5701a8a8a75127677a663fac53
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-01-29 15:06:02 -0500

    cleanup and comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,16 @@
 set -x EDITOR vim
 set -x PATH $PATH:$HOME/bin
 
-# fundle plugins
+# fundle plugins ----------
+
+# work with bash source scripts
 fundle plugin 'edc/bass'
+
+# add !! syntax
 fundle plugin 'oh-my-fish/plugin-bang-bang'
 
-fundle init
-# end fundle plugins
+# end fundle plugins ------
+fundle init # --profile # show plugin load time
 
-# auto run nvm use and making it "quiet"
-nvm use default >> /dev/null
+# auto run nvm use and make it "quiet"
+# nvm use default >> /dev/null
