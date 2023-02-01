fab695357910232ce4ef8af1b21031d9dc364770
Guy Bianco IV <gjbiancoiv@gmail.com>
2023-02-01 18:06:21 -0500

    simplify fish: remove most plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,15 +6,5 @@ fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
 
 alias sk="java -jar $HOME/code/gls/scaffolding/tools/sk/target/scaffolding.jar"
 alias vale="$HOME/bin/vale/bin/vale"
-alias valesk="vale content/**/*.adoc"
-alias valediff='vale (git diff --name-only main | grep -e ".*\.adoc\$")'
 
-# fundle plugins ----------
-fundle plugin 'edc/bass'
-fundle plugin 'oh-my-fish/plugin-bang-bang'
-fundle plugin 'techwizrd/fishmarks'
-# end fundle plugins ------
-fundle init # --profile # show plugin load time
-
-. ~/.config/fish/fundle/techwizrd/fishmarks/marks.fish
-. $HOME/.fishmarks/marks.fish
+. ~/.config/fish/functions/fishmarks.fish
