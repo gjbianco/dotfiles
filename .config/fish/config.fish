a08bd30b52b3366819bb76e2d10725e10cbce300
Guy Bianco IV <gjbiancoiv@gmail.com>
2023-02-01 12:23:44 -0500

    add rust ; remove go

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,8 @@
 set -x EDITOR vim
 fish_add_path $HOME/bin
 fish_add_path /opt/homebrew/bin
-fish_add_path $HOME/go/bin
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
 
 alias sk="java -jar $HOME/code/gls/scaffolding/tools/sk/target/scaffolding.jar"
 alias vale="$HOME/bin/vale/bin/vale"
