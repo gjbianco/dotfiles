1a5c3652b0f45a11daaf07a4e6648cdf9b6a9c0c
Guy Bianco IV <gjbiancoiv@gmail.com>
2023-09-10 19:50:39 -0400

    add fish aliases for work shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,5 +6,8 @@ fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
 
 alias sk="java -jar $HOME/code/gls/scaffolding/tools/sk/target/scaffolding.jar"
 alias vale="$HOME/bin/vale/bin/vale"
+alias wb="sk flamel && rm -rf guides/tmp && flamel sg"
+alias ws="scp 'guides/tmp/en-US/pdf/*.pdf' guys-macbook-air:Desktop"
+alias wp="open 'guides/tmp/en-US/pdf/*.pdf' &"
 
 . ~/.config/fish/functions/fishmarks.fish
