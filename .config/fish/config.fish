3de855e69fd0daba4af09df792323b6a0a920697
Guy Bianco IV <gjbiancoiv@gmail.com>
2023-11-03 14:54:17 -0400

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,14 @@
 set -x EDITOR vim
 fish_add_path $HOME/bin
 fish_add_path /opt/homebrew/bin
-fish_add_path $HOME/.cargo/bin
-fish_add_path $HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.local/bin
 
-alias sk="java -jar $HOME/code/gls/scaffolding/tools/sk/target/scaffolding.jar"
 alias vale="$HOME/bin/vale/bin/vale"
 alias wb="sk flamel && rm -rf guides/tmp && flamel sg"
 alias ws="scp 'guides/tmp/en-US/pdf/*.pdf' guys-macbook-air:Desktop"
-alias wp="open 'guides/tmp/en-US/pdf/*.pdf' &"
+alias wp="zathura 'guides/tmp/en-US/pdf/*.pdf' &"
+alias kp="keepassxc-cli"
 
 . ~/.config/fish/functions/fishmarks.fish
+. ~/.config/fish/functions/lab_completion.fish
