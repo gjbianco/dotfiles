8d57b2eedc39890758650b43f97997bf0d50c901
Guy Bianco IV <gjbiancoiv@gmail.com>
2024-03-10 20:37:16 -0400

    add restic path ; add reaper path ; add yt-dlp+sponsorblock alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,16 @@
 set -x EDITOR vim
+set -x RESTIC_REPOSITORY "sftp:rsync:data-backup"
 fish_add_path $HOME/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.local/bin
+fish_add_path $HOME/REAPER/REAPER
 
 alias vale="$HOME/bin/vale/bin/vale"
 alias wb="sk flamel && rm -rf guides/tmp && flamel sg"
 alias ws="scp 'guides/tmp/en-US/pdf/*.pdf' guys-macbook-air:Desktop"
 alias wp="zathura 'guides/tmp/en-US/pdf/*.pdf' &"
-alias kp="keepassxc-cli"
+alias yt="yt-dlp --sponsorblock-remove intro,preview,interaction,sponsor --sponsorblock-mark all"
 
 . ~/.config/fish/functions/fishmarks.fish
 . ~/.config/fish/functions/lab_completion.fish
