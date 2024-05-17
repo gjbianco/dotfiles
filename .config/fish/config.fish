be80d1b21ad80aac6e3a727d597db55dd31116e5
Guy Bianco IV <gjbiancoiv@gmail.com>
2024-05-17 13:37:47 -0400

    add bgm alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,6 +11,7 @@ alias wb="sk flamel && rm -rf guides/tmp && flamel sg"
 alias ws="scp 'guides/tmp/en-US/pdf/*.pdf' guys-macbook-air:Desktop"
 alias wp="zathura 'guides/tmp/en-US/pdf/*.pdf' &"
 alias yt="yt-dlp --sponsorblock-remove intro,preview,interaction,sponsor --sponsorblock-mark all"
+alias bgm="mpv . --shuffle --loop --no-resume-playback"
 
 . ~/.config/fish/functions/fishmarks.fish
 . ~/.config/fish/functions/lab_completion.fish
