4fd5f29c21a554968ee51440f222e701fd94cc35
Guy Bianco IV <gjbiancoiv@gmail.com>
2025-01-21 14:28:50 -0500

    fish aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,10 +8,16 @@ fish_add_path $HOME/REAPER/REAPER
 
 alias vale="$HOME/bin/vale/bin/vale"
 alias wb="sk flamel && rm -rf guides/tmp && flamel sg"
-alias ws="scp 'guides/tmp/en-US/pdf/*.pdf' guys-macbook-air:Desktop"
+alias wsm="scp 'guides/tmp/en-US/pdf/*.pdf' macbook:Desktop"
 alias wp="zathura 'guides/tmp/en-US/pdf/*.pdf' &"
 alias yt="yt-dlp --sponsorblock-remove intro,preview,interaction,sponsor --sponsorblock-mark all"
 alias bgm="mpv . --shuffle --loop --no-resume-playback"
+alias m="aerc"
+alias got="grc go test -v"
+alias n="newsboat"
+
+function f
+  find . -iname "*$argv*"
+end
 
 . ~/.config/fish/functions/fishmarks.fish
-. ~/.config/fish/functions/lab_completion.fish
